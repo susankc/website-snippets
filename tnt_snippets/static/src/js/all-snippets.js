2406baca69824456ac74ddeb7a8e4ebed1398a52
Asmit <mail2asmitnepali@gmail.com>
2018-11-25 15:37:30 +0545

    [FIX] tnt_snippets: Fix slider pointer click.
    Here we create slide on click on slider pointer.

--- tnt_snippets/static/src/js/all-snippets.js
+++ tnt_snippets/static/src/js/all-snippets.js
@@ -5,19 +5,20 @@ jQuery(document).ready(function($) {
             interval: 5000
     });
     $('[id^=carousel-selector-]').click(function () {
-    var id_selector = $(this).attr("id");
-    try {
-        var id = /-(\d+)$/.exec(id_selector)[1];
-        console.log(id_selector, id);
-        jQuery('#mygalleryCarousel').carousel(parseInt(id));
-    } catch (e) {
-        console.log('Regex failed!', e);
-    }
-});
+        var id_selector = $(this).attr("id");
+        try {
+            var id = /-(\d+)$/.exec(id_selector)[1];
+            console.log(id_selector, id);
+            jQuery('#mygalleryCarousel').carousel(parseInt(id));
+        } catch (e) {
+            console.log('Regex failed!', e);
+        }
+    });
     $('#mygalleryCarousel').on('slid.bs.carousel', function (e) {
              var id = $('.item.active').data('slide-number');
             $('#carousel-text').html($('#slide-content-'+id).html());
     });
+    console.log('Im load asmit');
 });
 
 //ISOTOPE GALLERY
